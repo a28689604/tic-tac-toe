4ed974c46a16951a10293aedf1313843401ed4b8
ericcchen <a28689604@gmail.com>
2022-11-03 09:14:56 +0800

    change git email test

--- src/Board.js
+++ src/Board.js
@@ -3,18 +3,21 @@ import Square from "./Square";
 const Board = (props) => {
   const renderSquare = (i) => {
     // IMPORTANT wrap up onClick, or it will cause infinity loop
-    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
+    return <Square key={i} value={props.squares[i]} onClick={() => props.onClick(i)} />;
   };
 
   let squareCounter = 0;
-
   const board = [];
   for (let row = 0; row < 3; row++) {
     const rowList = [];
     for (let col = 0; col < 3; col++) {
       rowList.push(renderSquare(squareCounter++));
     }
-    board.push(<div className="board-row">{rowList}</div>);
+    board.push(
+      <div key={row} className="board-row">
+        {rowList}
+      </div>
+    );
   }
 
   return <>{board}</>;
